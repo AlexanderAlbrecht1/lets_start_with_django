86ac30dca5b6abb1a3cf7813f9b9cb449e49300e
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-02 13:53:53 +0200

    slugify

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import start_page_view, test_page, single_gadget_view
+from .views import start_page_view, test_page, single_gadget_view, single_gadget_slug_view
 
 urlpatterns = [
     path( '', start_page_view),
     path('test', test_page),
-    path('single_gadget', single_gadget_view)
+    path('single_gadget/<int:gadget_id>', single_gadget_view),
+    path('single_gadget/<slug:gadget_slug>', single_gadget_slug_view)
 ]
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+import json
+from django.utils.text import slugify
 
 from .dummy_data import gadgets
 
@@ -11,5 +13,14 @@ def start_page_view(request):
 def test_page(request):
     return HttpResponse('Test works!')
 
-def single_gadget_view(request):
-    return HttpResponse(gadgets[0])
+def single_gadget_view(request, gadget_id):
+    return JsonResponse({"result": slugify(gadgets[gadget_id]['name'])})
+
+def single_gadget_slug_view(request, gadget_slug):
+    gadget_match = {"resukt": "nothing"}
+
+    for gadget in gadgets:
+        if slugify(gadget['name']) == gadget_slug:
+            gadget_match = gadget
+
+    return JsonResponse(gadget_match)
